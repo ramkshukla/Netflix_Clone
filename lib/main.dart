d471b41605cd1fe97b2d3dea9420ce5c7dac476a
iramkshukla <ramkumarshukla00000@gmail.com>
2022-10-26 18:44:16 +0530

    added detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:netflix_clone/my_app.dart';
+import 'package:netflix_clone/utils/text.dart';
+import 'package:netflix_clone/widgets/toprated.dart';
+import 'package:netflix_clone/widgets/trending.dart';
+import 'package:netflix_clone/widgets/tv.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.green),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final String apikey = 'dd3b117ce6223771ee6c788ea1d73678';
+  final String readaccesstoken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDNiMTE3Y2U2MjIzNzcxZWU2Yzc4OGVhMWQ3MzY3OCIsInN1YiI6IjYzNTE3ZGI2Yzk5ODI2MDA3ZGRjM2Q0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LVPHeenru-xdqqg6Iue7WWGu3VC9N5q9PJythEkeZyI';
+  List trendingmovies = [];
+  List topratedmovies = [];
+  List tv = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadmovies();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(
+      ApiKeys(apikey, readaccesstoken),
+      logConfig: const ConfigLogger(
+        showLogs: true,
+        showErrorLogs: true,
+      ),
+    );
+
+    Map trendingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvresult = await tmdbWithCustomLogs.v3.tv.getPouplar();
+    print((trendingresult));
+    setState(() {
+      trendingmovies = trendingresult['results'];
+      topratedmovies = topratedresult['results'];
+      tv = tvresult['results'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: modified_text(text: 'Flutter Movie App ❤️'),
+          backgroundColor: Colors.transparent,
+        ),
+        body: ListView(
+          children: [
+            TV(tv: tv),
+            TrendingMovies(
+              trending: trendingmovies,
+            ),
+            TopRatedMovies(
+              toprated: topratedmovies,
+            ),
+          ],
+        ));
+  }
 }
